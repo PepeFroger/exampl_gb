4931b362116bd992c1d14b064d35191596ae0188
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-10 15:06:03 +0300

    Сделал номер 2

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+Random rnd = new Random();
+int N = 5;
+int RandMas(){
+    int mas_i;
+    mas_i= rnd.Next(100,999);
+    return mas_i;
+}
+int count = 0;
+int [] mas = new int [N];
+for (int i=0; i<N;i++){
+    mas[i] = RandMas();
+    Console.Write($"{mas[i]}\t");
+    if(mas[i]%2==0){
+        count++;
+    }
+}
+Console.WriteLine($"\n{count}");
+
