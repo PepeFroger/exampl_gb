579ae41ce4e02ca4d6f8d3cf8883e70daec6e892
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-10 22:12:36 +0300

    Сделал 1 задание

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+int EndlessCycl(int i){
+    int num = Convert.ToInt32(Console.ReadLine());
+    int x = num, count=0;
+    while(x>0){
+        x = x/10;
+        count++;
+    }
+    if ((num%10+num/(10*count))%2 != 0){
+        i++;
+        return i;
+    }else{
+        return i;
+    }
+}
+int i =0;
+while(i == 0){
+    if(EndlessCycl(i) == 1){
+        i++;
+    }
+}
