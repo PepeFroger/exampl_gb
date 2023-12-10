d02db74d70a93104a65feaa3b01679c5f85f9892
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-10 16:05:51 +0300

    Исправил номер 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,19 +1,21 @@
 ﻿﻿using System;
 Random rnd = new Random();
 int N = 5;
-int RandMas(){
-    int mas_i;
-    mas_i= rnd.Next(100,999);
-    return mas_i;
-}
-int count = 0;
+int count;
 int [] mas = new int [N];
 for (int i=0; i<N;i++){
-    mas[i] = RandMas();
+    mas[i] = rnd.Next(100,999);
     Console.Write($"{mas[i]}\t");
-    if(mas[i]%2==0){
-        count++;
-    }
 }
-Console.WriteLine($"\n{count}");
-
+Console.WriteLine();
+int CountEven(int [] mas){
+    int count = 0;
+    for(int i=0;i<N;i++){
+        if(mas[i]%2==0){
+            count++;
+        }
+    }
+    return count;
+}
+count = CountEven(mas);
+Console.Write(count);
